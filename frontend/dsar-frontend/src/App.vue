09a5ba1d9fb98240ce2c9b58a30304a5d2de9bd7
ashokp1731-glitch <ashokp.1731@gmail.com>
2026-03-13 13:51:42 -0500

    Refactor App.vue to simplify structure and add inputs

--- frontend/dsar-frontend/src/App.vue
+++ frontend/dsar-frontend/src/App.vue
@@ -1,47 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<h1>DSAR Portal</h1> 
+<h2>Submit Request</h2> 
+<input v-model="customerName" placeholder="Customer Name" />
+<br /><br /> 
+<input v-model="email" placeholder="Email" />
+<br /><br /> 
+<select v-model="requestType"> 
+  <option>ACCESS</option> 
+  <option>DELETE</option> 
+  <option>CORRECT</option> 
+</select> 
+<br /><br />
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<button @click="submitRequest">Submit Request</button> 
+<hr /> 
+<h2>All Requests (Admin)</h2> 
+<button @click="loadRequests">Load Requests</button> 
+<ul> 
+  <li v-for="r in requests" :key="r.id"> 
+    {{ r.customerName }} - {{ r.requestType }} - {{ r.status }} 
+  </li> 
+</ul>
