9327e1925ff5849241f5533ca63f48f7ebda573f
pokuriashok1 <ashokkumar.pokuri1@gmail.com>
2026-03-13 12:37:58 -0500

    Added Spring boot backend project

---
+++ backend/dsar_backend/DsarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsarBackendApplication.class, args);
+	}
+
+}
