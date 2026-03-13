97e618d2f64ed588f65fe4047c8acafb5bde8812
pokuriashok1 <ashokkumar.pokuri1@gmail.com>
2026-03-13 12:42:00 -0500

    Added full Spring boot backend project

---
+++ backend/dsar-backend/src/test/java/com/example/dsar_backend/DsarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
